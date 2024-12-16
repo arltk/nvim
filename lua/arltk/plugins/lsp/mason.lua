6a9edb2885c0ca716e8be734b822aa95a88c1b88
Kaden Arlt <arltk@protonmail.com>
2024-12-16 00:47:36 -0600

    formatting plugin (lsp extension for formatting)

--- lua/arltk/plugins/lsp/mason.lua
+++ lua/arltk/plugins/lsp/mason.lua
@@ -1,44 +1,57 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "emmet_ls",
-        "lua_ls",
-        "prismals",
-        "pyright",
-        "bashls",
-        "dockerls",
-        "jsonls",
-        "nginx-language-server",
-        "powershell_es",
-        "yamlls",
-        "ansiblels",
-        "terraformls",
-      },
-    })
-  end,
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"emmet_ls",
+				"lua_ls",
+				"prismals",
+				"pyright",
+				"bashls",
+				"dockerls",
+				"jsonls",
+				"nginx-language-server",
+				"powershell_es",
+				"yamlls",
+				"ansiblels",
+				"terraformls",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"xmlformatter", -- xml formatter
+				"yamlfmt", -- yaml formatter
+			},
+		})
+	end,
 }
